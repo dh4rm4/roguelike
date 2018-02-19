5b578aa4155242379778c9e14ca2610d07cc6d50
dh4rm4_win <dh4rm4@protonmail.com>
2018-02-19 12:16:00 -0800

    Add loader + update gameManager

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
     public BoardManager boardScript;
 
-    private int level = 3; //starting level
+    private int level = 15; //starting level
 
     void Awake()
     {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
