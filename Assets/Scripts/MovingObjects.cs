9c6ea0a86efd45adaa36c1520ae34cfb7a9cc3f2
dh4rm4_win <dh4rm4@protonmail.com>
2018-03-04 10:34:33 -0800

    update

--- Assets/Scripts/MovingObjects.cs
+++ Assets/Scripts/MovingObjects.cs
@@ -1,16 +1,72 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingObjects : MonoBehaviour {
+public abstract class MovingObject : MonoBehaviour
+{
+    public float moveTime = 0.1f;
+    public LayerMask blockLayer;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+    }
+
+
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockLayer);
+        boxCollider.enabled = true;
+
+        if (hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+        return false;
+    }
+
+
+    protected virtual void AttemptMove <T>(int xDir, int yDir)
+        where T : Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+
+        if (hit.transform == null)
+            return;
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+            OnCantMove(hitComponent);
+    }
+
+
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        float sqrRemainDistance = (transform.position - end).sqrMagnitude;
+
+        // Find next point where we move on the vector (start -> end)
+        // move to this new point
+        while (sqrRemainDistance > float.Epsilon)
+        {
+            Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPostion);
+            sqrRemainDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    protected abstract void OnCantMove<T>(T component)
+        where T : Component;
 }
